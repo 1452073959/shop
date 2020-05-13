62d3fa0b4d82d33e9173247820c532d4cefdab79
cc <1452073959@qq.com>
2020-05-13 10:37:06 +0800

    商品属性

--- public/js/app.js
+++ public/js/app.js
@@ -53855,8 +53855,8 @@ Vue.component('user-addresses-create-and-edit', {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\laragon\www\shop\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\laragon\www\shop\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! F:\laragon\www\shop\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! F:\laragon\www\shop\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
